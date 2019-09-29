d5cafb7908d04132f8a766a06039406189d05fbe
sxiaolu86 <34197113+sxiaolu86@users.noreply.github.com>
2019-09-29 17:22:31 -0600

    Add files via upload

--- fetch.js
+++ fetch.js
@@ -3,10 +3,10 @@
 
 // API from https://rapidapi.com
 
-function searchMenu(zipCode){
+function searchMenu(zipCode, page){
   let url1 = 'https://us-restaurant-menus.p.rapidapi.com/restaurants/zip_code/';
-  let url2 = '?page=1';
-  const url = url1 + zipCode + url2;
+  let url2 = '?page=';
+  const url = url1 + zipCode + url2 + page;
   let settings = {
     "async": true,
     "crossDomain": true,
@@ -19,23 +19,54 @@ function searchMenu(zipCode){
   }
   
   $.ajax(settings).done(function (response) {
+    let cuisinesStr = '';
+    if(response.result.data[1].cuisines.length <= 0){
+      cuisinesStr = '*******';
+    }else{
+      cuisinesStr = response.result.data[1].cuisines.join();
+    }
+  
     console.log(response);
-
-
+    console.log(cuisinesStr);
+    displayPageNav(response);
   });
   }
 
+function displayPageNav(response){
+  let numOfPages = response.result.pages;
+  let numOfMenus = response.result.totalResults;
+  let zipCode = response.result.data[0].address.postal_code;
+  let htmlStr = `<h2>${numOfMenus} menus on file for zip code [${zipCode}] </h2>`;
+  let pageStr = `<p>(50 menus per page)`
+  for(let i=0; i<numOfPages; i++){
+    let str = `<button class="page-button" onclick="getPage(${i+1})">${i+1}</button>`;
+    htmlStr += str;
+  }
+  if(numOfPages>1){
+    htmlStr += pageStr;
+  }
 
+  document.getElementById('js-page-selector').innerHTML = htmlStr;
+}
+
+function getPage(i){
+  console.log(i);
+  showRestaurantCards(i);
+  
+}
+
+function showRestaurantCards(i){
+
+}
 
 // API key + URL from https://www.zip-codes.com/
 const zipApiKey = 'RFHNTCY2KK2J45YBFI6U'; 
 const zipSearchURL = 'https://api.zip-codes.com/ZipCodesAPI.svc/1.0/GetZipCodeDetails/';
 
 function searchZipCode(zipCode) {
-    console.log('in the searchZipCode method')
+
     let str = zipSearchURL + zipCode + '?';
     const url = str + 'key=' + zipApiKey;
-  
 
     fetch(url)
       .then(response => {
@@ -46,7 +77,7 @@ function searchZipCode(zipCode) {
       })
       .then(responseJson => displayZipResults(responseJson, url))
       .catch(err => {
-        $('#js-h3-messege').text(`Something went wrong: ${err.message}`);
+        $('#js-h3-messege').text(`No result found. Please try another zip code.`);
       });
   }
 
@@ -55,33 +86,34 @@ function searchZipCode(zipCode) {
     let str = 'Zip code found in the city of ' +responseJson.item.City;
     $('#js-h3-messege').text(str);
     //$('#js-h3-messege').text(`Zip code found!`);
-    $('#js-span-ZipCode').text(''+responseJson.item.ZipCode);
-    $('#js-span-AreaLand').text(''+responseJson.item.AreaLand);
-    $('#js-span-ZipCodePopulation').text(''+responseJson.item.ZipCodePopulation);
-    $('#js-span-region').text(''+responseJson.item.Region);
-    $('#js-span-Division').text(''+responseJson.item.Division);
-    $('#js-span-State').text(''+responseJson.item.State);
-    $('#js-span-CountyName').text(''+responseJson.item.CountyName);
-    $('#js-span-CountiesArea').text(''+responseJson.item.CountiesArea);
-    $('#js-span-Elevation').text(''+responseJson.item.Elevation);
-    $('#js-span-MalePop').text(''+responseJson.item.MalePop);
-    $('#js-span-FemalePop').text(''+responseJson.item.FemalePop);
-    $('#js-span-WhitePop').text(''+responseJson.item.WhitePop);
-    $('#js-span-BlackPop').text(''+responseJson.item.BlackPop);
-    $('#js-span-HispanicPop').text(''+responseJson.item.HispanicPop);
-    $('#js-span-AsianPop').text(''+responseJson.item.AsianPop);
-    $('#js-span-HawaiianPop').text(''+responseJson.item.HawaiianPop);
-    $('#js-span-IndianPop').text(''+responseJson.item.IndianPop);
-    $('#js-span-OtherPop').text(''+responseJson.item.OtherPop);
-    $('#js-span-MedianAge').text(''+responseJson.item.MedianAge);
-    $('#js-span-MedianAgeMale').text(''+responseJson.item.MedianAgeMale);
-    $('#js-span-MedianAgeFemale').text(''+responseJson.item.MedianAgeFemale);
-    $('#js-span-HouseholdsPerZipcode').text(''+responseJson.item.HouseholdsPerZipcode);
-    $('#js-span-AverageFamilySize').text(''+responseJson.item.AverageFamilySize);
-    $('#js-span-PersonsPerHousehold').text(''+responseJson.item.PersonsPerHousehold);
-    $('#js-span-AverageHouseValue').text(''+responseJson.item.AverageHouseValue);
-    $('#js-span-IncomePerHousehold').text(''+responseJson.item.IncomePerHousehold);
-    $('#js-span-ZIncomePerHouseholdipCode').text(''+responseJson.item.ZIncomePerHouseholdipCode);
-    $('#js-span-Bus03Establishments').text(''+responseJson.item.Bus03Establishments);
+    $('#js-ZipCode').text(''+responseJson.item.ZipCode);
+    $('#js-AreaLand').text(''+responseJson.item.AreaLand);
+    $('#js-ZipCodePopulation').text(''+responseJson.item.ZipCodePopulation);
+    $('#js-region').text(''+responseJson.item.Region);
+    $('#js-Division').text(''+responseJson.item.Division);
+    $('#js-State').text(''+responseJson.item.State);
+    $('#js-CountyName').text(''+responseJson.item.CountyName);
+    $('#js-CountiesArea').text(''+responseJson.item.CountiesArea);
+    $('#js-Elevation').text(''+responseJson.item.Elevation);
+    $('#js-MalePop').text(''+responseJson.item.MalePop);
+    $('#js-FemalePop').text(''+responseJson.item.FemalePop);
+    $('#js-WhitePop').text(''+responseJson.item.WhitePop);
+    $('#js-BlackPop').text(''+responseJson.item.BlackPop);
+    $('#js-HispanicPop').text(''+responseJson.item.HispanicPop);
+    $('#js-AsianPop').text(''+responseJson.item.AsianPop);
+    $('#js-HawaiianPop').text(''+responseJson.item.HawaiianPop);
+    $('#js-IndianPop').text(''+responseJson.item.IndianPop);
+    $('#js-OtherPop').text(''+responseJson.item.OtherPop);
+    $('#js-MedianAge').text(''+responseJson.item.MedianAge);
+    $('#js-MedianAgeMale').text(''+responseJson.item.MedianAgeMale);
+    $('#js-MedianAgeFemale').text(''+responseJson.item.MedianAgeFemale);
+    $('#js-HouseholdsPerZipcode').text(''+responseJson.item.HouseholdsPerZipcode);
+    $('#js-AverageFamilySize').text(''+responseJson.item.AverageFamilySize);
+    $('#js-PersonsPerHousehold').text(''+responseJson.item.PersonsPerHousehold);
+    $('#js-AverageHouseValue').text(''+responseJson.item.AverageHouseValue);
+    $('#js-IncomePerHousehold').text(''+responseJson.item.IncomePerHousehold);
+    $('#js-ZIncomePerHouseholdipCode').text(''+responseJson.item.ZIncomePerHouseholdipCode);
+    $('#js-Bus03Establishments').text(''+responseJson.item.Bus03Establishments);
+    showResultCards(true);
   };
 
--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 'use strict';
-//****************** data storage & click listeners */
+//****************** screen switches & click listeners */
 
 //****************** click listeners ******************************** */
 function watchForms() {
     $('#js-form-zip').submit(event => {
         event.preventDefault();
         const userInput = $('#js-input-zip').val();
-        //searchZipCode(userInput);
-        searchMenu(userInput);
+        searchZipCode(userInput);
+        searchMenu(userInput,1);
     });
 }
 
 $(function() {
+    showResultCards(false);
     watchForms();
+
 });
 
+function showResultCards(switchOn){
+    if(switchOn){
+        $('#zip-info-1').show();
+        $('#zip-info-2').show();
+        $('#zip-info-3').show();
+    }else{
+        $('#zip-info-1').hide();
+        $('#zip-info-2').hide();
+        $('#zip-info-3').hide();
+    }
 
+}
 
 //****************** HTML writters ******************************** */
